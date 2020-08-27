a991501ba2f5a0715aabf75c68b95232553c2e57
Ararat Mendez <ararat.mendez@gmail.com>
2020-08-27 15:41:41 -0600

    cifrado AES

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+
+// RSA
+
 const NodeRSA = require('node-rsa');
 
 cryptRSA();
@@ -32,3 +35,30 @@ function cryptRSA() {
     console.log("======================== FIN PROCESO DE CIFRADO ==========================");
 
 }
+
+// AES
+
+const crypto = require('crypto');
+const key = "MySecretKeyUltimate";
+let value = encode("Mensaje ultra secreto", key);
+
+function encode(text, key) {
+    console.log("========================= ENCRIPTAR AES ===========================");
+	const mykey = crypto.createCipher('aes-128-cbc', key);
+	let mystr = mykey.update(text, 'utf8', 'hex');
+	mystr += mykey.final('hex');
+	return(mystr);
+}
+
+console.log('El mensaje cifrado es: ' + value );
+
+function decode(message, key) {
+    console.log("========================= DESENCRIPTAR AES ===========================");
+	const mykey = crypto.createDecipher('aes-128-cbc', key);
+	let mystr = mykey.update(message, 'hex', 'utf8');
+	mystr += mykey.final('utf8');
+	return(mystr);
+}
+
+value = decode(value, key);
+console.log('El mensaje original es: ' + value);
